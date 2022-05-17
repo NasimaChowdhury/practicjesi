35730edf6d6a875118dcebcba75788f5a7ec9396
NasimaCHOWDHURY <nsmchwdhry@gmail.com>
2022-05-16 22:09:31 -0400

    First Commit for Self practic

---
+++ src/javaBasicJesi/Hello.java
@@ -0,0 +1,12 @@
+package javaBasicJesi;
+
+public class Hello {
+	//main
+	public static void main(String[] args) {
+	// syso
+		System.out.println("Hello Jesi! Welcome to Java!");
+	
+	}
+}
+
+//This part is Editor
